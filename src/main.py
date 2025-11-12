273453db7c1d3a1b6b3fdac13bdb2755a14f0433
Kalyuzhnyy Dmitry <difrom_pro@mail.ru>
2025-11-12 17:11:32 +0300

    add tests

--- src/main.py
+++ src/main.py
@@ -13,3 +13,16 @@ def calculate_taxes(prices: list[float], tax_rate: float) -> list[float]:
         taxed_prices.append(price + tax)
 
     return taxed_prices
+
+
+def calculate_tax(price: float, tax_rate: float, discount: int|float=0, point: int=2) -> float:
+    if not isinstance(discount, float|int) or not isinstance(point, int):
+       raise ValueError('Неправильные параметры')
+
+    if price <= 0:
+        raise ValueError('Неверная цена')
+
+    if not 0 <= tax_rate <= 100:
+        raise ValueError('Неверный налоговый процент')
+    tax = price * tax_rate / 100
+    return price+tax
--- tests/test_calculate.py
+++ tests/test_calculate.py
@@ -1,20 +1,52 @@
 import pytest
 
 from src.main import calculate_taxes
+from src.main import calculate_tax
+
+@pytest.fixture()
+def prices():
+    return [100, 200, 300]
 
 
-@pytest.mark.parametrize('prices, taxes, expected',
-                         [([10, 40, 100], 10, [11, 44, 110.0])])
-def test_calculate_taxes(prices, taxes, expected):
-    assert (calculate_taxes(prices,taxes)) == expected
+@pytest.mark.parametrize('tax_rate, expected',
+                         [( 10, [110, 220, 330]),
+                          ( 20, [120, 240, 360])])
+def test_calculate_taxes(prices, tax_rate, expected):
+    assert (calculate_taxes(prices,tax_rate)) == expected
 
 
-def test_calculate_zero_negativ():
+def test_calculate_zero_negativ(prices):
     with pytest.raises(ValueError) as exc_info:
         calculate_taxes([-10, 10, 10, 0], 10)
     assert str(exc_info.value) == 'Неверная цена'
 
     with pytest.raises(ValueError) as exc_info:
-        calculate_taxes([10, 10, 10], -10)
+        calculate_taxes(prices, -10)
     assert str(exc_info.value) == 'Неверный налоговый процент'
+
+
+@pytest.mark.parametrize('price, tax_rate, expected',[(100, 10, 110),
+                                                          (50, 5, 52.5)])
+def test_calculate_tax(price, tax_rate, expected):
+    assert (calculate_tax(price,tax_rate)) == expected
+
+@pytest.mark.parametrize('price',[(-1),
+                                   (0)])
+def test_calculate_tax_price_negative(price):
+    with pytest.raises(ValueError) as exc_info:
+        calculate_tax(price, 1)
+
+def test_calculate_tax_tax_rate_negative():
+        with pytest.raises(ValueError) as exc_info:
+            calculate_tax(1, -1)
+
+@pytest.mark.parametrize('discount, point',[("1", 1),
+                                   (1, " ")])
+def test_calculate_tax_type_invalid(discount, point):
+    with pytest.raises(ValueError) as exc_info:
+        calculate_tax(1, 1, discount, point)
+
+
+
+
 
